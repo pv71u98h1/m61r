f9a97ace48e64990f9e80eecbf95ac68737e02d1
pv71u98h1 <pv71u98h1@gmail.com>
2022-05-06 13:47:04 +0200

    add join for m61r obj

--- R/m61r_utils.R
+++ R/m61r_utils.R
@@ -32,3 +32,88 @@ dim.m61r <- function(x,...){
 as.data.frame.m61r <- function(x,...){
   get("values",x)()
 }
+
+rbind.m61r <- function(x,...){
+  datasets <- list(x,...)
+  check_class <- lapply(datasets,function(x) inherits(x,"m61r"))
+  check_class <- prod(unlist(check_class))
+  if (check_class==1L) {
+    res <- do.call("rbind",lapply(datasets,function(x){
+      x[]
+    }))
+  } else stop("arguments in '...' are not of class m61r")
+  return(m61r::m61r(res))
+}
+
+cbind.m61r <- function(x,...){
+  datasets <- list(x,...)
+  check_class <- lapply(datasets,function(x) inherits(x,"m61r"))
+  check_class <- prod(unlist(check_class))
+  if (check_class==1L) {
+    res <- do.call("cbind",lapply(datasets,function(x){
+      x[]
+    }))
+  } else stop("arguments in '...' are not of class m61r")
+  return(m61r::m61r(res))
+}
+
+left_join <- function(x,y,by=NULL,by.x=NULL,by.y=NULL){
+  datasets <- list(x,y)
+  check_class <- lapply(datasets,function(x) inherits(x,"m61r"))
+  check_class <- prod(unlist(check_class))
+  if (check_class==1L) {
+    res <- left_join_(x[],y[],by=NULL,by.x=NULL,by.y=NULL)
+  } else stop("arguments in '...' are not of class m61r")
+  return(m61r::m61r(res))
+}
+
+right_join <- function(x,y,by=NULL,by.x=NULL,by.y=NULL){
+  datasets <- list(x,y)
+  check_class <- lapply(datasets,function(x) inherits(x,"m61r"))
+  check_class <- prod(unlist(check_class))
+  if (check_class==1L) {
+    res <- right_join_(x[],y[],by=by,by.x=by.x,by.y=by.y)
+  } else stop("arguments in '...' are not of class m61r")
+  return(m61r::m61r(res))
+}
+
+inner_join <- function(x,y,by=NULL,by.x=NULL,by.y=NULL){
+  datasets <- list(x,y)
+  check_class <- lapply(datasets,function(x) inherits(x,"m61r"))
+  check_class <- prod(unlist(check_class))
+  if (check_class==1L) {
+    res <- inner_join_(x[],y[],by=by,by.x=by.x,by.y=by.y)
+
+  } else stop("arguments in '...' are not of class m61r")
+  return(m61r::m61r(res))
+}
+
+full_join <- function(x,y,by=NULL,by.x=NULL,by.y=NULL){
+  datasets <- list(x,y)
+  check_class <- lapply(datasets,function(x) inherits(x,"m61r"))
+  check_class <- prod(unlist(check_class))
+  if (check_class==1L) {
+    res <- full_join_(x[],y[],by=by,by.x=by.x,by.y=by.y)
+  } else stop("arguments in '...' are not of class m61r")
+  return(m61r::m61r(res))
+}
+
+semi_join <- function(x,y,by=NULL,by.x=NULL,by.y=NULL){
+  datasets <- list(x,y)
+  check_class <- lapply(datasets,function(x) inherits(x,"m61r"))
+  check_class <- prod(unlist(check_class))
+  if (check_class==1L) {
+    res <- semi_join_(x[],y[],by=by,by.x=by.x,by.y=by.y)
+  } else stop("arguments in '...' are not of class m61r")
+  return(m61r::m61r(res))
+}
+
+anti_join <- function(x,y,by=NULL,by.x=NULL,by.y=NULL){
+  datasets <- list(x,y)
+  check_class <- lapply(datasets,function(x) inherits(x,"m61r"))
+  check_class <- prod(unlist(check_class))
+  if (check_class==1L) {
+    res <- anti_join_(x[],y[],by=by,by.x=by.x,by.y=by.y)
+  } else stop("arguments in '...' are not of class m61r")
+  return(m61r::m61r(res))
+}
